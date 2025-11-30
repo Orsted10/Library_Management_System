a64fb5165c593ddc0188a68dace5a5443e83c515
Ankan Bhattacharjee <113756091+Orsted10@users.noreply.github.com>
2025-11-30 19:00:51 +0530

    Code Push - Ankan

---
+++ len_den.h
@@ -0,0 +1,8 @@
+#ifndef LENDEN_H
+#define LENDEN_H
+#include "common.h"
+//Book issue karne ka header hai So koi book issue aye easily ho sake
+void kitabUthao(Kitab *shelf);
+//Wapas karne ka header hai, easy access ke liye as above
+void kitabWapasKaro(Kitab *shelf);
+#endif
