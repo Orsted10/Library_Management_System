20a2970887d31eccb752b8c22073005262e57a51
Aditya Yadav <ketanyadav8329@gmail.com>
2025-11-30 15:21:36 +0530

    Aditya_Update_1

---
+++ helper.h
@@ -0,0 +1,8 @@
+#ifndef HELPER_H
+#define HELPER_H
+#include "common.h"
+// ID se search karke index return karega
+int dhundoKitabIDSe(Kitab *shelf,int targetID);
+// Author ya title se search
+void searchByName(Kitab *shelf);
+#endif
