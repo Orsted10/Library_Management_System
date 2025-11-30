e180fb09e64307d878e6af65bdb3445a8507a360
Ankan Bhattacharjee <113756091+Orsted10@users.noreply.github.com>
2025-11-30 22:55:25 +0530

    Fix to accommodate with new structure of program

---
+++ godam.h
@@ -0,0 +1,10 @@
+#ifndef GODAM_H
+#define GODAM_H
+#include "common.h"
+// Book add karne ka function
+void nayiKitabDalo(Kitab *shelf);
+// Sab dikhane ka function
+void sabDikhao(Kitab *shelf);
+// Initialization
+void setupLibrary(Kitab *shelf);
+#endif
